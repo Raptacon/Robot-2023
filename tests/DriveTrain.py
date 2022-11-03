dd0c09efc3dc67c3c17043ca3a8e33a43d5b7a63
DarthCooper <LoganBarber1@outlook.com>
2022-11-02 19:58:10 -0600

    started BaseCodeRewrite

---
+++ tests/DriveTrain.py
@@ -0,0 +1,84 @@
+from enum import Enum, auto
+import ctre
+from cmath import sqrt
+import logging as log
+from networktables import NetworkTables
+
+class ControlMode(Enum):
+    """
+    Drive Train Control Modes
+    """
+    kArcadeDrive = auto()
+    kTankDrive = auto()
+    kSwerveDrive = auto()
+    kDisabled = auto()
+
+class DriveTrain():
+    motors_driveTrain: dict
+    gearRatio = 10
+    driveMotorsMultiplier = .7
+    creeperMotorsMultiplier = .25
+
+    smartDashTable = NetworkTables.getTable("SmartDashboard")
+
+    def setup(self):
+        self.motorSpeedInfo = {}
+        self.creeperMode = False
+        log.info("DriveTrain setup completed")
+
+    def setMotorSpeeds(self, motorSpeedInfo:dict):
+        self.motorSpeedInfo = motorSpeedInfo
+
+    def setBraking(self, braking:bool):
+        if braking:
+            for motor in self.motors_driveTrain.keys():
+                self.motors_driveTrain[motor].setNeutralMode(ctre.NeutralMode.Brake)
+        else:
+            for motor in self.motors_driveTrain.keys():
+                self.motors_driveTrain[motor].setNeutralMode(ctre.NeutralMode.Coast)
+
+    def getSpecificMotor(self, motorName):
+        try:
+            return self.motors_driveTrain[motorName].get()
+        except:
+            return
+
+    def setDriveMotorsMultiplier(self, MotorsMultiplier:float):
+        self.driveMotorsMultiplier = MotorsMultiplier
+
+    def setcreeperMotorsMultiplier(self, MotorsMultiplier:float):
+        self.creeperMotorsMultiplier = MotorsMultiplier
+
+    def stop(self):
+        self.motorSpeedInfo = {}
+        for key in self.motors_driveTrain.keys():
+            self.motorSpeedInfo[key] = 0
+
+    def enableCreeperMode(self):
+        """when left bumper is pressed, it sets the driveMotorsMultiplier to .25"""
+        if self.creeperMode:
+            return
+        self.prevMultiplier = self.driveMotorsMultiplier
+        self.driveMotorsMultiplier = self.creeperMotorsMultiplier
+        self.creeperMode = True
+
+    def disableCreeperMode(self):
+        """when left bumper is released, it sets the multiplier back to it's original value"""
+        if not self.creeperMode:
+            return
+        self.driveMotorsMultiplier = self.prevMultiplier
+        self.creeperMode = False
+
+    def execute(self):
+
+        # Make sure motors are the same between parameter information and drivetrain
+        # then set motors
+        speedInfoKeys = sorted(dict(self.motorSpeedInfo).keys())
+        driveTrainKeys = sorted(self.motors_driveTrain.keys())
+        if speedInfoKeys != driveTrainKeys:
+            print("not matching")
+            self.stop()
+            speedInfoKeys = sorted(dict(self.motorSpeedInfo).keys())
+
+        for key in speedInfoKeys:
+            self.motors_driveTrain[key].set(self.motorSpeedInfo[key])
