ecff07dc7af974a794bbd6113b85fc8298c30b81
Leif Creer <leifcreer@gmail.com>
2023-01-20 18:49:19 -0700

    Inverted motors instead of controllers

--- robots/greenBot.py
+++ robots/greenBot.py
@@ -31,8 +31,10 @@ class GreenBot(commands2.TimedCommandRobot):
         rightM = wpilib.MotorControllerGroup(motors['right'], motors['rightF'])
         leftM = wpilib.MotorControllerGroup(motors['left'], motors['leftF'])
 
-        self.driveTrain = Drivetrain(rightM, leftM, motors['left'], motors['right'], None)
-        self.tankDrive = TankDrive(getStick(wpilib.XboxController.Axis.kRightY, True),
+        rightM.setInverted(True)
+
+        self.driveTrain = Drivetrain(rightM, leftM, motors['left'], motors['right'], navx.AHRS.create_i2c())
+        self.tankDrive = TankDrive(getStick(wpilib.XboxController.Axis.kRightY, False),
                                    getStick(wpilib.XboxController.Axis.kLeftY, False),
                                    self.driveTrain)
         self.arcadeDrive = ArcadeDrive(getStick(wpilib.XboxController.Axis.kLeftY, True),
