8639dbf52dc77b0771ad36580ca760a0f9f9da9a
DarthCooper <LoganBarber1@outlook.com>
2023-01-27 18:11:34 -0700

    Created Motor helper

--- robots/greenBot.py
+++ robots/greenBot.py
@@ -6,6 +6,7 @@ from subsystems.drivetrains.westcoast import Westcoast as Drivetrain
 from commands.tankDrive import TankDrive
 from commands.arcadeDrive import ArcadeDrive
 import wpimath.filter
+from MotorHelper import createMotor
 import wpimath
 
 import enum
@@ -17,21 +18,44 @@ class GreenBot(commands2.TimedCommandRobot):
         ARCADE = enum.auto()
         TANK = enum.auto()
 
+    class motorDescription():
+        """
+        Here is where all the basic info for a motor is set up.
+        """
+        def __init__(self, type, channel, usesPID, P, I, D, haslimits, CurrentLimit, triggerCurrent, triggerTime, invert, masterChannel):
+            self.motorType = type
+            self.motorChannel = channel
+            self.motorUsesPID = usesPID
+            self.motorP = P
+            self.motorI = I
+            self.motorD = D
+            self.motorHasLimits = haslimits
+            self.triggerThresholdCurrent = triggerCurrent
+            self.triggerThresholdTime = triggerTime
+            self.currentLimit = CurrentLimit
+            self.motorInvert = invert
+            self.motorMasterChannel = masterChannel
 
     def __init__(self, period: float = 0.02) -> None:
         super().__init__(period)
 
         #create the greenbot motors
+        type = "CANTalonFX"
         motors = {}
-        motors['right'] = ctre.WPI_TalonFX(30)
-        motors['rightF'] = ctre.WPI_TalonFX(31)
-        motors['left'] = ctre.WPI_TalonFX(20)
-        motors['leftF'] = ctre.WPI_TalonFX(21)
+        rightMotor = self.motorDescription("CANTalonFX", 30, False, 0, 0, 0, True, 40, 60, 50, False, 0)
+        rightMotorFront = self.motorDescription("CANTalonFX", 31, False, 0, 0, 0, True, 40, 60, 50, False, 0)
+        leftMotor = self.motorDescription("CANTalonFX", 20, False, 0, 0, 0, True, 40, 60, 50, False, 0)
+        leftMotorFront = self.motorDescription("CANTalonFX", 21, False, 0, 0, 0, True, 40, 60, 50, False, 0)
 
-        rightM = wpilib.MotorControllerGroup(motors['right'], motors['rightF'])
-        leftM = wpilib.MotorControllerGroup(motors['left'], motors['leftF'])
+        rightMotor = createMotor(rightMotor, motors)
+        rightMotorFront = createMotor(rightMotorFront, motors)
+        leftMotor = createMotor(leftMotor, motors)
+        leftMotorFront = createMotor(leftMotorFront, motors)
 
-        self.driveTrain = Drivetrain(rightM, leftM, motors['left'], motors['right'], navx.AHRS.create_i2c())
+        rightM = wpilib.MotorControllerGroup(rightMotor, rightMotorFront)
+        leftM = wpilib.MotorControllerGroup(leftMotor, leftMotorFront)
+
+        self.driveTrain = Drivetrain(rightM, leftM, leftMotor, rightMotor, navx.AHRS.create_i2c())
         self.tankDrive = TankDrive(getStick(wpilib.XboxController.Axis.kRightY, True),
                                    getStick(wpilib.XboxController.Axis.kLeftY, False),
                                    self.driveTrain)
