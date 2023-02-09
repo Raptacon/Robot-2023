a9a2586a01a3df1158fed93de4e6da96fdd91022
Nathan Beasley <tuffinmuffin@gmail.com>
2023-02-08 19:09:05 -0700

    pep8 fix

--- robots/configBasedRobot.py
+++ robots/configBasedRobot.py
@@ -16,7 +16,7 @@ class  ConfigBaseCommandRobot(commands2.TimedCommandRobot):
 
         assert config, "Please configure default robotConfig. \n\
                         run 'echo (robotCfg.yml) > robotConfig' on roborio\n\
-                        where (robotCfg.yml) is the name of the file"    
+                        where (robotCfg.yml) is the name of the file"
 
         self.configMapper = utils.configMapper.ConfigMapper(config, configPath)
 
