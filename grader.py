7969a30d70f7e180685a8826d10ea32b1ab94372
Leif Creer <leifcreer@gmail.com>
2023-02-22 18:12:42 -0700

    cleaned up somethings

--- grader.py
+++ grader.py
@@ -14,22 +14,10 @@ class Grader(commands2.SubsystemBase):
         print(kwargs)
 
         if len(kargs) > 0:
-            self.winchM = kargs[0] if len(kargs) > 0 else None
+            self.graderM = kargs[0] if len(kargs) > 0 else None
             #self.winchEncoder = kargs[1] if len(kargs) > 1 else None
-            if not (self.winchM):
-                raise Exception("winch motor must be provided")
+            if not (self.graderM):
+                raise Exception("grader motor must be provided")
 
         else:
-            self.winchM = hwFactory.getHardwareComponet("Arm" , "winch")
-
-            #self.winchEncoder = self.winchM.getEncoder() if isinstance(self.winchM, rev.CANSparkMax) else None
-"""
-    def log(self):
-        '''
-        Log telemetry to smartdashboard
-        '''
-        if self.winchEncoder:
-            sensor = self.winchEncoder.getSensorCollection()
-           wpilib.SmartDashboard.putNumber("Winch Distance", sensor.getIntegratedSensorAbsolutePosition())
-           wpilib.SmartDashboard.putNumber("Winch Speed", sensor.getIntegratedSensorVelocity())
-"""
+            self.graderM = hwFactory.getHardwareComponet("Arm" , "Grader")
