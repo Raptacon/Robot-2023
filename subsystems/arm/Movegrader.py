f315a303acd8c62d89c7d6e0fa29d7b011faae03
Leif Creer <leifcreer@gmail.com>
2023-02-24 19:28:26 -0700

    updated some things

--- subsystems/arm/Movegrader.py
+++ subsystems/arm/Movegrader.py
@@ -5,15 +5,19 @@ from typing import Callable
 log = logging.getLogger(__name__)
 
 class MoveGrader(commands2.CommandBase):
-    def __init__(self,graderMotorTriger: Callable[[], float],graderMotorTriger: Callable[[], bollean], grader):
+    def __init__(self,graderMotorTriger: Callable[[], float],graderMotorBumper: Callable[[], float], grader):
         super().__init__()
-        self.graderMotor = winchMotor
+        self.intake = graderMotorTriger
+        self.outtake = graderMotorBumper
         self.grader = grader
         self.addRequirements(self.winch)
 
     def execute(self) -> None:
         graderMotor = self.graderMotor()
-        self.grader.setspeed(graderMotor)
+        if self.outtake == True:
+            self.grader.setspeed(1)
+        elif self.intake == True:
+            self.grader.setspeec(-1)
 
     def end(self, interrupted: bool) -> None:
         self.grader.setspeed(0)
