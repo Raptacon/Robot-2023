3986767e5f3cc766b9a07ab43e2ce00e9ca70b4f
DarthCooper <LoganBarber1@outlook.com>
2023-03-08 20:21:37 -0700

    Added some files so this actually runs

---
+++ subsystems/actuators/breadboxWinch.py
@@ -0,0 +1,61 @@
+import commands2
+import logging
+import utils
+import wpilib
+import wpilib.interfaces
+hwFactory = utils.hardwareFactory.getHardwareFactory()
+import utils.sensorFactory
+import utils.motorHelper
+
+log = logging.getLogger("winch")
+
+class Winch(commands2.CommandBase):
+    motor: wpilib.interfaces._interfaces.MotorController
+    encoder: wpilib.DutyCycleEncoder
+    wayPoint = 0
+    def __init__(self, *kargs, **kwargs) -> None:
+        super().__init__()
+        motorSettings = {
+            "type":"SparkMax",
+            "inverted": False,
+            "motorType": "kBrushless",
+            "sensorPhase": True,
+            "channel": 43
+        }
+        self.motor = utils.motorHelper.createMotor(motorSettings)
+        encoderSettings = {
+            "type": "wpilib.DutyCycleEncoder",
+            "channel": 1,
+            "offset": 0.0,
+            "unitsPerRotation": 6.28318530718,
+            "minDutyCycle": 0.0,
+            "maxDutyCycle": 1.0}
+
+        self.encoder = utils.sensorFactory.create("wpilib.DutyCycleEncoder", encoderSettings)
+        self.encoder.reset()
+
+
+    def setDistance(self, distance : float):
+        self.wayPoint = distance
+        if self.encoder.getPositionOffset() > self.wayPoint:
+            self.forward = False
+        else:
+            self.forward = True
+
+    def execute(self) -> None:
+        self.distance = self.encoder.getPositionOffset()
+        if self.forward and self.distance < self.wayPoint:
+            self.motor.setVoltage(8)
+        elif not self.forward and self.distance > self.wayPoint:
+            self.motor.setVoltage(-8)
+
+    def end(self, interrupted: bool) -> None:
+        self.motor.setVoltage(0)
+        self.encoder.reset()
+
+    def isFinished(self) -> bool:
+        if self.forward and self.distance >= self.wayPoint:
+            return True
+        elif not self.forward and self.distance <= self.wayPoint:
+            return True
+        return False
