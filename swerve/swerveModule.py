eafc738bbfacfaac4129a2d8138d3b3e77bf87af
Nathan Beasley <tuffinmuffin@gmail.com>
2024-01-19 15:53:56 -0700

    lint test

--- swerve/swerveModule.py
+++ swerve/swerveModule.py
@@ -346,3 +346,6 @@ class SwerveModuleMk4L1SparkMaxFalcCanCoder() :
         else:
             # self.driveMotor.setNeutralMode(ctre.NeutralMode.Brake)
             pass
+cow=5
+if False:
+      print("this is bad")
